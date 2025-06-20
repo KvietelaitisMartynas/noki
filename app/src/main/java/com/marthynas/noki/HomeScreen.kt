bb07153a4f3aa3a122e67abf4d8da0eb742a02ae
martynas <martynaskvietelaitis@icloud.com>
2025-06-20 16:42:56 +0300

    added screens

---
+++ app/src/main/java/com/marthynas/noki/HomeScreen.kt
@@ -0,0 +1,54 @@
+package com.marthynas.noki
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.marthynas.noki.ui.theme.NokiTheme
+
+@Composable
+fun HomeScreen(navController: NavController, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "Home Screen")
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { navController.navigate("details") }) {
+            Text(text = "Go to Details")
+        }
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview() {
+    NokiTheme {
+        HomeScreen(rememberNavController())
+    }
+}
--- app/src/main/java/com/marthynas/noki/MainActivity.kt
+++ app/src/main/java/com/marthynas/noki/MainActivity.kt
@@ -4,13 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.marthynas.noki.ui.theme.NokiTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,12 +33,22 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NokiTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                val navController = rememberNavController()
+                NavHost(navController, startDestination = "home") {
+                    composable("home") {
+                        HomeScreen(navController)
+                    }
+                    composable("details") {
+                        DetailsScreen(navController)
+                    }
+                }
+
+/*                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    GreetingScreen(
+                        name = "Martynas",
                         modifier = Modifier.padding(innerPadding)
                     )
-                }
+                }*/
             }
         }
     }
@@ -38,10 +62,38 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+@Composable
+fun StartingTextField()
+{
+    var textState by remember { mutableStateOf("") }
+    TextField(
+        value = textState,
+        onValueChange = { textState = it },
+        label = { Text("Name") }
+    )
+}
+
+@Composable
+fun GreetingScreen(name: String, modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Greeting(name, modifier)
+        Spacer(Modifier.height(32.dp))
+        StartingTextField()
+    }
+}
+
+
+/*
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     NokiTheme {
-        Greeting("Android")
+        GreetingScreen("Martynas");
     }
-}
+}*/
